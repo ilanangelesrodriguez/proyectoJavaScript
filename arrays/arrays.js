ab2c5b3c954ccae45b6785326bb5b0fa36ff5518
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-10-08 11:03:57 -0500

    Agrego ejemplo de Arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,15 @@
+var frutas=["Manzana","Platano","Cereza","Fresa"];
+console.log(frutas);
+console.log(frutas.length)
+console.log(frutas.push("Naranja"))
+console.log(frutas);
+console.log(frutas.pop());
+console.log(frutas);
+console.log(frutas.unshift("Naranja"));
+console.log(frutas)
+//Eliminar el primer elemento del array
+var shift=frutas.shift();
+console.log(shift)
+//Devolver posicion de un elemento
+var indexOf=frutas.indexOf("Manzana");
+console.log(indexOf)
